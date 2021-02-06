9a174f0ae6bc3484a692b1e57d1bba05fb565742
bonnielu <bonnielu2002@gmail.com>
2021-02-06 16:05:47 -0500

    flask backend and react frontend framework

--- western_ai/api/app.py
+++ western_ai/api/app.py
@@ -1,7 +1,8 @@
+import time
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "hi"
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
